0506b1a417d345b43b8378d14b5d2c6d1fad152c
Egor <petruhin-enot@yandex.ru>
2023-06-17 08:53:18 +0400

    Added license

--- gfb.go
+++ gfb.go
@@ -1,3 +1,14 @@
+// Copyright (C) Egor
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// GNU Lesser General Public Licence is available at
+// http://www.gnu.org/copyleft/lesser.html
 package gfb
 
 import (
